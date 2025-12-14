5678e443259004ee3ed37d489703a79f51ecc0eb
Muhammet Esat Demir <muhammetesatdemir@outlook.com>
2025-12-14 18:10:10 +0300

    Add project files.

---
+++ JwtAuthDotNet9/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthDotNet9.Models
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
