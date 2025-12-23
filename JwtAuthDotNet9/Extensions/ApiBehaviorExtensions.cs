0f8fc8d8a42341b4d1442e09495f8c4fcfaf408f
Muhammet Esat Demir <muhammetesatdemir@outlook.com>
2025-12-23 17:53:56 +0300

    apibehavior ve jwtauth classlara ayrılması

---
+++ JwtAuthDotNet9/Extensions/ApiBehaviorExtensions.cs
@@ -0,0 +1,40 @@
+﻿using JwtAuthDotNet9.Models.BaseModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDotNet9.Extensions
+{
+    public static class ApiBehaviorExtensions
+    {
+        public static IServiceCollection AddCustomApiBehavior(
+            this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var traceId = context.HttpContext.TraceIdentifier;
+
+                    var errors = context.ModelState
+                        .Where(x => x.Value!.Errors.Count > 0)
+                        .ToDictionary(
+                            x => x.Key,
+                            x => x.Value!.Errors
+                                .Select(e => e.ErrorMessage)
+                                .ToArray()
+                        );
+
+                    var response = ApiResponse<object>.Fail(
+                        message: "Validation error",
+                        errors: errors,
+                        traceId: traceId
+                    );
+
+                    return new BadRequestObjectResult(response);
+                };
+            });
+
+            return services;
+        }
+    }
+
+}
--- JwtAuthDotNet9/Program.cs
+++ JwtAuthDotNet9/Program.cs
@@ -20,106 +20,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-//builder.Services.AddRateLimiter(options =>
-//{
-//    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-
-//    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
-//    {
-//        var ip = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
-
-//        return RateLimitPartition.GetFixedWindowLimiter(
-//            partitionKey: ip,
-//            factory: _ => new FixedWindowRateLimiterOptions
-//            {
-//                PermitLimit = 100,
-//                Window = TimeSpan.FromMinutes(1),
-//                QueueLimit = 10,
-//                QueueProcessingOrder = QueueProcessingOrder.OldestFirst
-//            });
-//    });
-//    options.AddFixedWindowLimiter("login", opt =>
-//    {
-//        opt.PermitLimit = 5;
-//        opt.Window = TimeSpan.FromMinutes(1);
-//        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-//        opt.QueueLimit = 2;
-//    });
-//});
-
 builder.Services.AddRateLimiting();
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddFluentValidationClientsideAdapters();
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = context =>
-    {
-        var traceId = context.HttpContext.TraceIdentifier;
-        var errors = context.ModelState
-            .Where(x => x.Value!.Errors.Count > 0)
-            .ToDictionary(
-                x => x.Key,
-                x => x.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
-            );
-
-        var response = ApiResponse<object>.Fail(
-            message: "Validation error",
-            errors: errors,
-            traceId: traceId
-        );
-
-        return new BadRequestObjectResult(response);
-    };
-});
+builder.Services.AddCustomApiBehavior();
 builder.Services.AddValidatorsFromAssemblyContaining<UserDtoValidator>();
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("UserDatabase")));
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["AppSettings:Issuer"],
-            ValidAudience = builder.Configuration["AppSettings:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(
-                System.Text.Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]))
-        };
-        
-        options.Events = new JwtBearerEvents
-        {
-            OnChallenge = context =>
-            {
-                context.HandleResponse();
-                var traceId = context.HttpContext.TraceIdentifier;
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                context.Response.ContentType = "application/json";
-
-                var response = ApiResponse<object>.Fail("Yetkisiz eriþim",traceId:traceId);
-
-                return context.Response.WriteAsJsonAsync(response);
-            },
-
-            OnForbidden = context =>
-            {
-                var traceId = context.HttpContext.TraceIdentifier;
-                context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                context.Response.ContentType = "application/json";
-
-                var response = ApiResponse<object>.Fail("Bu iþlem için yetkin yok", traceId: traceId);
-
-                return context.Response.WriteAsJsonAsync(response);
-            }
-        };
-    });
-
+builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Services.AddScoped<IAuthService, AuthService>();
 var app = builder.Build();
 
